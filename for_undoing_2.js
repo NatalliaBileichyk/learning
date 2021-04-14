45a814aa4a803b3dcedf223abfce28d57c08007b
Bileichyk, Natallia <Natallia.Bileichyk@wolterskluwer.com>
2021-04-14 10:45:18 +0300

    clean code

--- for_undoing_2.js
+++ for_undoing_2.js
@@ -1,9 +1,9 @@
 function old (parameter){
     if (parameter < 10) {
-console.log("little")
-} else if (parameter < 14) {
-    console.log("baby")
-        } else {console.log("older")};
+        console.log("little");
+    } else if (parameter < 14) {
+                console.log("baby");
+            } else {console.log("older");}
     }
 
-old(55);
+old(7);
