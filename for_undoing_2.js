663abedf6fcff7fbfe27a52bb6ecf7ec04010467
Bileichyk, Natallia <Natallia.Bileichyk@wolterskluwer.com>
2021-04-14 17:55:06 +0300

    change parameter

--- for_undoing_2.js
+++ for_undoing_2.js
@@ -1,7 +1,7 @@
 function old (parameter){
     if (parameter < 3) {
         console.log("little baby");
-    } else if (parameter < 50) {
+    } else if (parameter < 450) {
                console.log("adult");
             } else {console.log("granny");}
     }
