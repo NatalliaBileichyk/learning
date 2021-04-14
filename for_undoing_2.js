542dc3f36d39acc4443d4dc77181e577d29811d4
Bileichyk, Natallia <Natallia.Bileichyk@wolterskluwer.com>
2021-04-14 10:31:05 +0300

    add

---
+++ for_undoing_2.js
@@ -0,0 +1,9 @@
+function old (parameter){
+    if (parameter < 3) {
+console.log("little")
+} else if (parameter < 50) {
+    console.log("baby")
+        } else {console.log("older")};
+    }
+
+old(55);
